0531cc73ef3971551f82d2df52cad9610c0eb6bb
cnb <carnav@gmail.com>
2014-04-18 21:53:31 +0200

    2.4.4.1 (today)
    - added Italian language sent by Gianpaolo ages ago
    - fix broken Dutch language (bug found by thyrif)

--- news_manager/lang/nl_NL.php
+++ news_manager/lang/nl_NL.php
@@ -60,9 +60,9 @@ $i18n = array(
 "LAST_SAVED"          =>  "Laatst Bijgewerkt",
 
 # validation
-"FIELD_IS_REQUIRED"   => "Dit veld is verplicht"
-"ENTER_VALID_DATE"    => "Vul een geldige datum in / Laat leeg voor de huidige datum"
-"ENTER_VALID_TIME"    => "Vul een geldige tijd in / Laat leeg voor de huidige tijd"
+"FIELD_IS_REQUIRED"   => "Dit veld is verplicht",
+"ENTER_VALID_DATE"    => "Vul een geldige datum in / Laat leeg voor de huidige datum",
+"ENTER_VALID_TIME"    => "Vul een geldige tijd in / Laat leeg voor de huidige tijd",
 
 # htaccess
 "HTACCESS_HELP"       =>  "Vervang voor het gebruik van Fancy URLs voor berichten, archieven, etc. de inhoud van het <code>.htaccess</code> bestand met onderstaande regels.",
